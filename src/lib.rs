bf0476c38becb42e5ed142887997ce70c2998d4f
Redmomn <109732988+Redmomn@users.noreply.github.com>
2025-01-24 14:11:51 +0800

    refactor: 使用kv类型描述protobuf

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ pub mod varint;
 mod tests {
     use super::*;
     use crate::buffer::Reader;
-    use crate::protobuf::{decode_protobuf, DataType, ProtoData, WireType};
+    use crate::protobuf::ProtoData::Message;
+    use crate::protobuf::{decode_protobuf, Map, ProtoData};
     use std::vec;
 
     #[test]
@@ -17,229 +18,103 @@ mod tests {
             "08d2fe061d424b1d002952bf0100000000003a0d3131343531343139313938313042047468697342026973420161420872657065617465644206737472696e674a10ec9962d372ce9be816be0b7fdea0127b5210292c88aab6b4386c5259b3db5bb3d83552106c67dcd74452b07dbab383af60213edc5210625b2652e828a520e2d3738fbc7623f952105c4627267501260e1635696f21df442352100e4f254dabe6afe218d47229213fce2e5210d61fea3ad5f6c7de29cb530974daebe85210bd7969536028618e616db54cc5bc17ab5210b82c3166710a0373d0fae7bfa5da6a405210aacb0a4e4eb2400cf3a9674cbd9fac30521056b3e167c737ac1d52770b789ba1e04a5210d99426dc60b67af15121a38816fa33c35210328fce15be2219c6cd950241d9db083652102db0c75a59f5527584b2924a32995f625210258d410b8fcf29b7c8405ae99560d43a5210eeafec9d7d49109a08d5d36f26e5f94852107aa31e3a68c0ea5cf197a9613a68de46",
         )
         .unwrap();
-        assert_eq!(
-            pb,
-            vec![
-                ProtoData {
-                    field: 1,
-                    wire_type: WireType::VARINT,
-                    data: DataType::Varint(114514),
-                },
-                ProtoData {
-                    field: 3,
-                    wire_type: WireType::I32,
-                    data: DataType::Fix32(1919810),
-                },
-                ProtoData {
-                    field: 5,
-                    wire_type: WireType::I64,
-                    data: DataType::Fix64(114514),
-                },
-                ProtoData {
-                    field: 7,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("1145141919810".to_string()),
-                },
-                ProtoData {
-                    field: 8,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("this".to_string())
-                },
-                ProtoData {
-                    field: 8,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("is".to_string())
-                },
-                ProtoData {
-                    field: 8,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("a".to_string())
-                },
-                ProtoData {
-                    field: 8,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("repeated".to_string())
-                },
-                ProtoData {
-                    field: 8,
-                    wire_type: WireType::LEN,
-                    data: DataType::String("string".to_string())
-                },
-                ProtoData {
-                    field: 9,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "ec 99 62 d3 72 ce 9b e8 16 be 0b 7f de a0 12 7b".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "29 2c 88 aa b6 b4 38 6c 52 59 b3 db 5b b3 d8 35".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "6c 67 dc d7 44 52 b0 7d ba b3 83 af 60 21 3e dc".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "62 5b 26 52 e8 28 a5 20 e2 d3 73 8f bc 76 23 f9".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "5c 46 27 26 75 01 26 0e 16 35 69 6f 21 df 44 23".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "0e 4f 25 4d ab e6 af e2 18 d4 72 29 21 3f ce 2e".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "d6 1f ea 3a d5 f6 c7 de 29 cb 53 09 74 da eb e8".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "bd 79 69 53 60 28 61 8e 61 6d b5 4c c5 bc 17 ab".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "b8 2c 31 66 71 0a 03 73 d0 fa e7 bf a5 da 6a 40".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "aa cb 0a 4e 4e b2 40 0c f3 a9 67 4c bd 9f ac 30".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "56 b3 e1 67 c7 37 ac 1d 52 77 0b 78 9b a1 e0 4a".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "d9 94 26 dc 60 b6 7a f1 51 21 a3 88 16 fa 33 c3".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "32 8f ce 15 be 22 19 c6 cd 95 02 41 d9 db 08 36".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "2d b0 c7 5a 59 f5 52 75 84 b2 92 4a 32 99 5f 62".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "25 8d 41 0b 8f cf 29 b7 c8 40 5a e9 95 60 d4 3a".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "ee af ec 9d 7d 49 10 9a 08 d5 d3 6f 26 e5 f9 48".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-                ProtoData {
-                    field: 10,
-                    wire_type: WireType::LEN,
-                    data: DataType::Bytes(
-                        hex::decode(
-                            "7a a3 1e 3a 68 c0 ea 5c f1 97 a9 61 3a 68 de 46".replace(" ", "")
-                        )
-                        .unwrap()
-                    )
-                },
-            ]
+
+        let mut expect_pb = Map::new();
+        expect_pb.insert(1, ProtoData::Varint(114514));
+        expect_pb.insert(3, ProtoData::Fix32(1919810));
+        expect_pb.insert(5, ProtoData::Fix64(114514));
+        expect_pb.insert(7, ProtoData::String("1145141919810".to_string()));
+        expect_pb.insert(
+            8,
+            ProtoData::Repeated(vec![
+                ProtoData::String("this".to_string()),
+                ProtoData::String("is".to_string()),
+                ProtoData::String("a".to_string()),
+                ProtoData::String("repeated".to_string()),
+                ProtoData::String("string".to_string()),
+            ]),
         );
+        expect_pb.insert(
+            9,
+            ProtoData::Bytes(
+                hex::decode("ec 99 62 d3 72 ce 9b e8 16 be 0b 7f de a0 12 7b".replace(" ", ""))
+                    .unwrap(),
+            ),
+        );
+        expect_pb.insert(
+            10,
+            ProtoData::Repeated(vec![
+                ProtoData::Bytes(
+                    hex::decode("29 2c 88 aa b6 b4 38 6c 52 59 b3 db 5b b3 d8 35".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("6c 67 dc d7 44 52 b0 7d ba b3 83 af 60 21 3e dc".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("62 5b 26 52 e8 28 a5 20 e2 d3 73 8f bc 76 23 f9".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("5c 46 27 26 75 01 26 0e 16 35 69 6f 21 df 44 23".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("0e 4f 25 4d ab e6 af e2 18 d4 72 29 21 3f ce 2e".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("d6 1f ea 3a d5 f6 c7 de 29 cb 53 09 74 da eb e8".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("bd 79 69 53 60 28 61 8e 61 6d b5 4c c5 bc 17 ab".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("b8 2c 31 66 71 0a 03 73 d0 fa e7 bf a5 da 6a 40".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("aa cb 0a 4e 4e b2 40 0c f3 a9 67 4c bd 9f ac 30".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("56 b3 e1 67 c7 37 ac 1d 52 77 0b 78 9b a1 e0 4a".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("d9 94 26 dc 60 b6 7a f1 51 21 a3 88 16 fa 33 c3".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("32 8f ce 15 be 22 19 c6 cd 95 02 41 d9 db 08 36".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("2d b0 c7 5a 59 f5 52 75 84 b2 92 4a 32 99 5f 62".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("25 8d 41 0b 8f cf 29 b7 c8 40 5a e9 95 60 d4 3a".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("ee af ec 9d 7d 49 10 9a 08 d5 d3 6f 26 e5 f9 48".replace(" ", ""))
+                        .unwrap(),
+                ),
+                ProtoData::Bytes(
+                    hex::decode("7a a3 1e 3a 68 c0 ea 5c f1 97 a9 61 3a 68 de 46".replace(" ", ""))
+                        .unwrap(),
+                ),
+            ]),
+        );
+
+        assert_eq!(pb, Message(expect_pb));
     }
 
-    fn read_protobuf(hex_str: &str) -> anyhow::Result<Vec<ProtoData>> {
+    fn read_protobuf(hex_str: &str) -> anyhow::Result<protobuf::ProtoData> {
         let bytes = hex::decode(hex_str.replace(" ", ""))?;
         decode_protobuf(&mut Reader::new(&bytes.as_slice()))
     }
--- src/protobuf.rs
+++ src/protobuf.rs
@@ -3,10 +3,12 @@ use crate::error::DecodeError;
 use crate::fixint::{read_fix32, read_fix64};
 use crate::varint::read_uvarint;
 use anyhow::Result;
+use std::collections::BTreeMap;
 use std::fmt::{Display, Formatter};
+use std::hash::{Hash, Hasher};
 use std::str;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum WireType {
     VARINT = 0, // int32, int64, uint32, uint64, sint32, sint64, bool, enum
     I64 = 1,    // fixed64, sfixed64, double
@@ -44,22 +46,119 @@ impl Display for WireType {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum DataType {
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+pub enum ProtoData {
     Varint(u64),
     Fix64(i64),
     Fix32(i32),
     Bytes(Vec<u8>),
     String(String),
-    Repeated(Vec<DataType>),
-    Message(Vec<ProtoData>),
+    Repeated(Vec<ProtoData>),
+    Message(Map<u64, ProtoData>),
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct ProtoData {
-    pub field: u64,
-    pub wire_type: WireType,
-    pub data: DataType,
+type MapImpl<K, V> = BTreeMap<K, V>;
+
+#[derive(Debug)]
+pub struct Map<K, V> {
+    map: MapImpl<K, V>,
+}
+
+impl Default for Map<u64, ProtoData> {
+    fn default() -> Self {
+        Map {
+            map: BTreeMap::new(),
+        }
+    }
+}
+
+impl Clone for Map<u64, ProtoData> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Map {
+            map: self.map.clone(),
+        }
+    }
+
+    #[inline]
+    fn clone_from(&mut self, source: &Self) {
+        self.map.clone_from(&source.map);
+    }
+}
+
+impl PartialEq for Map<u64, ProtoData> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.map.eq(&other.map)
+    }
+}
+
+impl Eq for Map<u64, ProtoData> {}
+
+impl Hash for Map<u64, ProtoData> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.map.hash(state);
+    }
+}
+
+impl Map<u64, ProtoData> {
+    pub fn new() -> Self {
+        Map {
+            map: MapImpl::new(),
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.map.clear();
+    }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.map.is_empty()
+    }
+
+    pub fn contains_key(&self, key: u64) -> bool {
+        self.map.contains_key(&key)
+    }
+
+    pub fn get(&self, key: u64) -> Option<&ProtoData> {
+        self.map.get(&key)
+    }
+
+    pub fn get_mut(&mut self, key: u64) -> Option<&mut ProtoData> {
+        self.map.get_mut(&key)
+    }
+
+    pub fn insert(&mut self, key: u64, message: ProtoData) -> Option<ProtoData> {
+        self.map.insert(key, message)
+    }
+
+    pub fn remove(&mut self, key: u64) -> Option<ProtoData> {
+        self.map.remove(&key)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&u64, &ProtoData)> {
+        self.map.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&u64, &mut ProtoData)> {
+        self.map.iter_mut()
+    }
+
+    pub fn keys(&self) -> impl Iterator<Item = &u64> {
+        self.map.keys()
+    }
+
+    pub fn values(&self) -> impl Iterator<Item = &ProtoData> {
+        self.map.values()
+    }
+
+    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut ProtoData> {
+        self.map.values_mut()
+    }
 }
 
 impl TryFrom<u64> for WireType {
@@ -84,8 +183,8 @@ pub fn read_tag(buf: &mut Reader) -> Result<(u64, WireType)> {
     Ok((tag >> 3, WireType::try_from(tag & 0x07)?))
 }
 
-pub fn read_length_delimited(buf: &mut Reader) -> Result<Vec<DataType>> {
-    let mut result: Vec<DataType> = Vec::new();
+pub fn read_length_delimited(buf: &mut Reader) -> Result<Vec<ProtoData>> {
+    let mut result: Vec<ProtoData> = Vec::new();
     let len = read_uvarint(buf)?;
 
     let mut data_buf = Reader::new(buf.read_bytes(len as usize)?);
@@ -93,7 +192,7 @@ pub fn read_length_delimited(buf: &mut Reader) -> Result<Vec<DataType>> {
     // 优先protobuf
     loop {
         match decode_protobuf(&mut data_buf) {
-            Ok(v) => result.push(DataType::Message(v)),
+            Ok(v) => result.push(v),
             Err(err) => match err.downcast_ref::<DecodeError>() {
                 Some(DecodeError::EOF) => return Ok(result),
                 _ => {
@@ -108,7 +207,7 @@ pub fn read_length_delimited(buf: &mut Reader) -> Result<Vec<DataType>> {
     // 转为str 可能会把varint也转换成str
     match str::from_utf8(data_buf.read_all_bytes()?) {
         Ok(v) => {
-            result.push(DataType::String(v.to_string()));
+            result.push(ProtoData::String(v.to_string()));
             return Ok(result);
         }
         _ => {
@@ -132,35 +231,45 @@ pub fn read_length_delimited(buf: &mut Reader) -> Result<Vec<DataType>> {
     //         }
     //     }
     // }
-    result.push(DataType::Bytes(Vec::from(data_buf.read_all_bytes()?)));
+    result.push(ProtoData::Bytes(Vec::from(data_buf.read_all_bytes()?)));
 
     Ok(result)
 }
 
-pub fn decode_protobuf(buf: &mut Reader) -> Result<Vec<ProtoData>> {
-    let mut parsed_data = Vec::<ProtoData>::new();
+pub fn decode_protobuf(buf: &mut Reader) -> Result<ProtoData> {
+    let mut parsed_data = Map::default();
     loop {
         match read_tag(buf) {
             Ok((field, wire_type)) => {
                 let data = match wire_type {
-                    WireType::VARINT => DataType::Varint(read_uvarint(buf)?),
-                    WireType::I64 => DataType::Fix64(read_fix64(buf)?),
-                    WireType::I32 => DataType::Fix32(read_fix32(buf)?),
+                    WireType::VARINT => ProtoData::Varint(read_uvarint(buf)?),
+                    WireType::I64 => ProtoData::Fix64(read_fix64(buf)?),
+                    WireType::I32 => ProtoData::Fix32(read_fix32(buf)?),
                     WireType::LEN => {
                         let mut list = read_length_delimited(buf)?;
                         if list.len() > 1 {
-                            DataType::Repeated(list)
+                            ProtoData::Repeated(list)
                         } else {
                             list.remove(0)
                         }
                     }
                     x => return Err(DecodeError::DeprecatedWireType(x).into()),
                 };
-                parsed_data.push(ProtoData {
-                    field,
-                    wire_type,
-                    data,
-                });
+                if parsed_data.contains_key(field) {
+                    match parsed_data.get(field) {
+                        None => {}
+                        Some(ProtoData::Repeated(_)) => {
+                            if let Some(ProtoData::Repeated(list)) = parsed_data.get_mut(field) {
+                                list.push(data);
+                            }
+                        }
+                        Some(v) => {
+                            parsed_data.insert(field, ProtoData::Repeated(vec![v.clone(), data]));
+                        }
+                    };
+                } else {
+                    parsed_data.insert(field, data);
+                }
             }
             Err(err) => match err.downcast_ref::<DecodeError>() {
                 Some(DecodeError::EOF) => break,
@@ -168,5 +277,5 @@ pub fn decode_protobuf(buf: &mut Reader) -> Result<Vec<ProtoData>> {
             },
         }
     }
-    Ok(parsed_data)
+    Ok(ProtoData::Message(parsed_data))
 }
